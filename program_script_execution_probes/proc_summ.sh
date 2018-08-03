dd61a25a419b5c22424f8551609ee670eab984a8
YK <yaroslav.kharitonov@rossinno.biz>
2018-08-03 12:39:30 +0300

    Number of found processes has been added.

--- program_script_execution_probes/proc_summ.sh
+++ program_script_execution_probes/proc_summ.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Summarizes %CPU, %MEM, VSZ and RSS for a process pvided as an arg.
+# Summarizes number of processes, %CPU, %MEM, VSZ and RSS for a process pvided as an arg.
 
-ps aux | grep -i $1 | grep -v grep | awk '{cpu += $3; mem += $4; vsz += $5; rss += $6} END { print "\{\"cpu\":\""cpu,"\",\"mem\":\""mem,"\",\"vsz\":"vsz,",\"rss\":"rss "\}" }'
+ps aux | grep -i $1 | grep -v grep | awk '{cpu += $3; mem += $4; vsz += $5; rss += $6} END { print "\{\"proc_num\":"NR,",\"cpu\":\""cpu,"\",\"mem\":\""mem,"\",\"vsz\":"vsz,",\"rss\":"rss "\}" }'
